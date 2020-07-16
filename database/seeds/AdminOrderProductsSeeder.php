436c564adf17a8ea5e2aec9337b8565f5652ef15
mrbuslov <buslovdmitrij0@gmail.com>
2020-07-16 15:20:01 +0300

    Create TSingletone, Registry, BlogApp

--- database/seeds/AdminOrderProductsSeeder.php
+++ database/seeds/AdminOrderProductsSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class AdminOrderProductsSeeder extends Seeder
 {
     /**
@@ -11,6 +12,24 @@ class AdminOrderProductsSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $data = [];
+
+
+        for ($y = 1; $y <= 12; $y++) {
+            for ($i = 1; $i <= 5; $i++) {
+                $data[] = [
+                    'order_id' => $y,
+                    'product_id' => $i,
+                    'qty' => rand(1,4),
+                    'title' => 'Casio Test Prod',
+                    'price' => rand(40,200),
+                ];
+            }
+        }
+
+
+        DB::table('order_products')->insert($data);
     }
+
 }
